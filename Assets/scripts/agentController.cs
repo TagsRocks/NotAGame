b1d01110582b346ac2a832e291c1b0048fbd7f17
minibotan <minibotan1996@gmail.com>
2016-09-01 22:18:34 +0300

    demo
    alfa-alfa version

---
+++ Assets/scripts/agentController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class agentController : MonoBehaviour {
+    private Transform goal;
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        findNewGoal();
+    }
+
+    void findNewGoal()
+    {
+        goal = GameObject.FindGameObjectsWithTag("agentGoal")[Random.Range(0, GameObject.FindGameObjectsWithTag("agentGoal").Length)].transform;
+        agent.destination = goal.position;
+    }
+
+
+    bool isNear()
+    {
+        return (transform.position - goal.transform.position).magnitude <= 3f;
+    }
+    // Update is called once per frame
+    void Update () {
+	    if(isNear())
+        {
+            findNewGoal();
+        }
+	}
+}
---
+++ Assets/scripts/controller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour
+{
+		
+
+		public float speed = 6.0F;
+		public float jumpSpeed = 20.0F;
+		public float gravity = 10.0F;
+		private Vector3 moveDirection = Vector3.zero;
+		
+		void Start(){}
+		
+		void Update() {
+			CharacterController controller = GetComponent<CharacterController>();
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump")){
+				//mycamera.jumpCount += 1f;
+				moveDirection.y = jumpSpeed;
+			}
+			moveDirection.y -= gravity * Time.deltaTime;
+			controller.Move(moveDirection * Time.deltaTime);
+		}
+		
+}
